a9a6d7f0ef01d44eb99041be8840726dbafed5bf
abigailhaddad <abigail.haddad@gmail.com>
2023-12-15 10:12:09 -0500

    add function-level documentation

--- code/functions.py
+++ code/functions.py
@@ -1,7 +1,6 @@
 import os
 import pandas as pd
 import random
-import re
 from litellm import completion
 from transformers import AutoTokenizer, AutoModel
 import torch
@@ -13,11 +12,33 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class DataLoader:
+    """
+    A class for loading data into a DataFrame.
+
+    Args:
+        input_data (str or list): The input data to be loaded. If `is_file_path` is True, it should be a file path to a CSV file. Otherwise, it should be a list of prompts.
+        is_file_path (bool, optional): Indicates whether `input_data` is a file path or a list of prompts. Defaults to True.
+
+    Methods:
+        load_data(): Loads the data into a DataFrame and performs some preprocessing.
+
+    Returns:
+        pandas.DataFrame: The loaded data.
+
+    """
+
     def __init__(self, input_data, is_file_path=True):
         self.input_data = input_data
         self.is_file_path = is_file_path
 
     def load_data(self):
+        """
+        Loads the data into a DataFrame and performs some preprocessing.
+
+        Returns:
+            pandas.DataFrame: The loaded data.
+
+        """
         if self.is_file_path:
             logging.info("Reading prompts from CSV file.")
             df = pd.read_csv(self.input_data, encoding='ISO-8859-1')
@@ -80,6 +101,21 @@ class LLMUtility:
 
 
 class PerturbationGenerator:
+    """
+    A class that generates perturbations for a given prompt using a perturbation model.
+
+    Attributes:
+        perturbation_model (str): The name of the perturbation model.
+        provider (str): The provider of the perturbation model.
+        num_perturbations (int): The number of perturbations to generate.
+        temperature (float): The temperature parameter for generating perturbations.
+
+    Methods:
+        get_perturbations(prompt): Generates perturbations for a given prompt.
+        parse_model_response(response): Parses the response from the perturbation model.
+        get_perturbations_for_all_prompts(prompts): Generates perturbations for multiple prompts.
+    """
+
     def __init__(self, perturbation_model, num_perturbations, temperature):
         self.perturbation_model = perturbation_model[0]
         self.provider = perturbation_model[1]
@@ -87,6 +123,15 @@ class PerturbationGenerator:
         self.temperature = temperature
 
     def get_perturbations(self, prompt):
+        """
+        Generates perturbations for a given prompt.
+
+        Args:
+            prompt (str): The prompt for which perturbations need to be generated.
+
+        Returns:
+            list: A list of perturbations for the given prompt.
+        """
         if self.num_perturbations == 0:
             return [prompt]
 
@@ -104,6 +149,15 @@ class PerturbationGenerator:
         return perturbations
 
     def parse_model_response(self, response):
+        """
+        Parses the response from the perturbation model.
+
+        Args:
+            response (dict): The response from the perturbation model.
+
+        Returns:
+            list: A list of perturbations parsed from the response.
+        """
         content = response['choices'][0]['message']['content']
         # Handling both numbered list and bullet points
         if content.startswith('1.') or content.strip().startswith('-'):
@@ -115,6 +169,15 @@ class PerturbationGenerator:
         return perturbations
 
     def get_perturbations_for_all_prompts(self, prompts):
+        """
+        Generates perturbations for multiple prompts.
+
+        Args:
+            prompts (list): A list of prompts for which perturbations need to be generated.
+
+        Returns:
+            dict: A dictionary mapping each prompt to a list of perturbations.
+        """
         perturbations_dict = {}
         for prompt in prompts:
             perturbations = [prompt]  # Include the original prompt
@@ -129,12 +192,50 @@ class PerturbationGenerator:
 
 
 class ModelResponseGenerator:
+    """
+    A class that generates model responses based on given instructions and prompts.
+
+    Args:
+        models_dict (dict): A dictionary mapping model names to their respective providers.
+        instructions (str): The instructions to be included in the message content sent to the model.
+        temperature (float or str): The temperature value to control the randomness of the model's output. 
+            If "variable", a random value between 0.0 and 1.0 will be used for each prompt.
+
+    Methods:
+        create_result_dict(model, original_prompt, actual_prompt, response, temperature, target_answer, keywords, true_or_false):
+            Creates a dictionary containing the result information for a single prompt.
+
+        process_single_prompt(model, provider, original_prompt, actual_prompt, instructions, temperature, target_answer=None, keywords=None, true_or_false=None):
+            Processes a single prompt by calling the model and returning the result dictionary.
+
+        process_prompts(df, perturbations_dict, num_runs):
+            Processes multiple prompts from a DataFrame, applying perturbations and running the models.
+
+    """
+
     def __init__(self, models_dict, instructions, temperature):
         self.models_dict = models_dict
         self.instructions = instructions
         self.temperature = temperature
     
     def create_result_dict(self, model, original_prompt, actual_prompt, response, temperature, target_answer, keywords, true_or_false):
+        """
+        Creates a dictionary containing the result information for a single prompt.
+
+        Args:
+            model (str): The name of the model.
+            original_prompt (str): The original prompt.
+            actual_prompt (str): The perturbed prompt.
+            response (dict): The response received from the model.
+            temperature (float): The temperature value used for generating the response.
+            target_answer (str or None): The target answer for the prompt.
+            keywords (str or None): The keywords associated with the prompt.
+            true_or_false (bool or None): Whether this is a prompt the model knows the answer to.
+
+        Returns:
+            dict: The result dictionary containing the prompt information and model response.
+
+        """
         generated_text = response['choices'][0]['message']['content']
         result = {
             'model': model,
@@ -151,7 +252,26 @@ class ModelResponseGenerator:
             result['true_or_false'] = true_or_false
         return result
 
-    def process_single_prompt(self, model, provider, original_prompt, actual_prompt, instructions, temperature, target_answer=None, keywords=None, true_or_false = None):
+    def process_single_prompt(self, model, provider, original_prompt, actual_prompt, instructions, temperature, target_answer=None, keywords=None, true_or_false=None):
+        """
+        Processes a single prompt by calling the model and returning the result dictionary.
+
+        Args:
+            model (str): The name of the model.
+            provider (str): The provider of the model.
+            original_prompt (str): The original prompt.
+            actual_prompt (str): The perturbed prompt.
+            instructions (str): The instructions to be included in the message content sent to the model.
+            temperature (float or str): The temperature value to control the randomness of the model's output.
+                If "variable", a random value between 0.0 and 1.0 will be used for each prompt.
+            target_answer (str or None): The target answer for the prompt.
+            keywords (str or None): The keywords associated with the prompt.
+            true_or_false (bool or None): Whether this is a prompt the model knows the answer to.
+
+        Returns:
+            dict: The result dictionary containing the prompt information and model response.
+
+        """
         # If temperature is "variable", randomize it; otherwise, use the fixed value
         temp_value = random.uniform(0.0, 1.0) if temperature == "variable" else temperature
 
@@ -165,6 +285,18 @@ class ModelResponseGenerator:
         return self.create_result_dict(model, original_prompt, actual_prompt, response, temp_value, target_answer, keywords, true_or_false)
 
     def process_prompts(self, df, perturbations_dict, num_runs):
+        """
+        Processes multiple prompts from a DataFrame, applying perturbations and running the models.
+
+        Args:
+            df (pd.DataFrame): The DataFrame containing the prompts.
+            perturbations_dict (dict): A dictionary mapping prompts to their respective perturbations.
+            num_runs (int): The number of times to run each perturbation.
+
+        Returns:
+            pd.DataFrame: The DataFrame containing the results of the model responses.
+
+        """
         results = []
         for index, row in df.iterrows():
             prompt = row['prompt']
@@ -184,7 +316,32 @@ class ModelResponseGenerator:
 
 
 class ResultAggregator:
+    """
+    Class to aggregate results based on specified columns.
+
+    Attributes:
+        None
+
+    Methods:
+        aggregate(df, metric_name, group_by_columns, result_columns):
+            Aggregates the results based on the specified columns.
+
+    """
+
     def aggregate(self, df, metric_name, group_by_columns, result_columns):
+        """
+        Aggregates the results based on the specified columns.
+
+        Args:
+            df (pandas.DataFrame): The input DataFrame containing the results.
+            metric_name (str): The name of the metric to sort the DataFrame by.
+            group_by_columns (list): The columns to group the results by.
+            result_columns (list): The columns to include in the aggregated results.
+
+        Returns:
+            pandas.DataFrame: The aggregated results DataFrame.
+
+        """
         df_sorted = df.sort_values(by=metric_name, ascending=False)
         grouped = df_sorted.groupby(group_by_columns)
         best_results = grouped.head(1).rename(columns={col: f'best_{col}' for col in result_columns})
@@ -193,16 +350,39 @@ class ResultAggregator:
         return best_worst_merged
 
 class SimilarityCalculator:
+    """
+    A class that calculates similarity scores between target texts and actual texts using a pre-trained model.
+    """
+
     def __init__(self, model_name):
         self.model_name = model_name
 
     def calculate_score(self, target_texts, actual_texts):
+        """
+        Calculates the similarity score between target texts and actual texts.
+
+        Args:
+            target_texts (list): List of target texts.
+            actual_texts (list): List of actual texts.
+
+        Returns:
+            float: Similarity score between 0 and 1.
+        """
         model, tokenizer = self.get_model(self.model_name)
         target_embeddings = self.encode_texts(target_texts, model, tokenizer)
         actual_embeddings = self.encode_texts(actual_texts, model, tokenizer)
         return 1 - cosine(target_embeddings[0], actual_embeddings[0])
 
     def perform_similarity_analysis(self, df):
+        """
+        Performs similarity analysis on a DataFrame.
+
+        Args:
+            df (pandas.DataFrame): DataFrame containing target_answer and response columns.
+
+        Returns:
+            pandas.DataFrame: DataFrame with similarity_score column added.
+        """
         if 'target_answer' in df.columns:
             logging.info("Performing similarity analysis.")
             df['similarity_score'] = df.apply(
@@ -213,11 +393,31 @@ class SimilarityCalculator:
         return df
 
     def get_model(self, model_name: str):
+        """
+        Retrieves the pre-trained model and tokenizer.
+
+        Args:
+            model_name (str): Name of the pre-trained model.
+
+        Returns:
+            tuple: Tuple containing the model and tokenizer.
+        """
         tokenizer = AutoTokenizer.from_pretrained(model_name)
         model = AutoModel.from_pretrained(model_name)
         return model, tokenizer
 
     def encode_texts(self, texts: list, model, tokenizer):
+        """
+        Encodes the texts using the pre-trained model and tokenizer.
+
+        Args:
+            texts (list): List of texts to encode.
+            model: Pre-trained model.
+            tokenizer: Tokenizer.
+
+        Returns:
+            torch.Tensor: Encoded text embeddings.
+        """
         encoded_input = tokenizer(texts, padding=True, truncation=True, return_tensors='pt')
         with torch.no_grad():
             model_output = model(**encoded_input)
@@ -225,6 +425,15 @@ class SimilarityCalculator:
         return embeddings
     
     def calculate_similarity_scores(self, df):
+        """
+        Calculates similarity scores for each row in the DataFrame.
+
+        Args:
+            df (pandas.DataFrame): DataFrame containing target_answer and response columns.
+
+        Returns:
+            pandas.Series: Series containing similarity scores.
+        """
         return df.apply(
             lambda row: self.calculate_score([row['target_answer']], [row['response']])
             if pd.notnull(row['target_answer']) else None, 
@@ -249,10 +458,32 @@ class KeywordMatchCalculator:
         )
 
 class LLMRatingCalculator:
+    """
+    Class for calculating ratings using the LLM evaluation model.
+
+    Attributes:
+        llm_evaluation_model (tuple): A tuple containing the LLM evaluation model.
+
+    Methods:
+        rate_response(row): Rates a response based on its similarity to the target answer.
+        calculate_ratings(df): Calculates ratings for a DataFrame.
+
+    """
+
     def __init__(self, llm_evaluation_model):
         self.llm_evaluation_model = llm_evaluation_model
 
     def rate_response(self, row):
+        """
+        Rates a response based on its similarity to the target answer.
+
+        Args:
+            row (pandas.Series): A row from a DataFrame containing 'target_answer' and 'response' columns.
+
+        Returns:
+            float: The rating of the response on a scale from 0 to 1, or None if the rating cannot be extracted.
+
+        """
         model, provider = self.llm_evaluation_model
         rating_prompt = f"Rate the following response on an integer scale from 0 to 10 based on its similarity to the target answer. Only return an integer, with no comments or punctuation \n\nTarget Answer: {row['target_answer']}\nResponse: {row['response']}\nRating:"
         response = LLMUtility.call_model(model, [{"role": "user", "content": rating_prompt}], provider, temperature=0.7)
@@ -264,6 +495,16 @@ class LLMRatingCalculator:
             return None
 
     def calculate_ratings(self, df):
+        """
+        Calculates ratings for a DataFrame.
+
+        Args:
+            df (pandas.DataFrame): The DataFrame containing 'target_answer' and 'response' columns.
+
+        Returns:
+            pandas.Series: A Series containing the calculated ratings.
+
+        """
         return df.apply(
             lambda row: self.rate_response(row)
             if pd.notnull(row['target_answer']) and pd.notnull(row['response']) else None, 
@@ -271,6 +512,26 @@ class LLMRatingCalculator:
         )
         
 class LLMAnalysisPipeline:
+    """
+    Class representing the LLM Analysis Pipeline.
+
+    Args:
+        input_data (str): Path to the input data file.
+        models_dict (dict): Dictionary containing the models for generating responses.
+        perturbation_model (str): Name of the perturbation model.
+        llm_evaluation_model (str): Name of the LLM evaluation model.
+        temperature (float): Temperature parameter for response generation.
+        num_runs (int): Number of runs for response generation.
+        is_file_path (bool): Flag indicating whether the input data is a file path.
+        similarity_model_name (str): Name of the similarity model.
+        num_perturbations (int): Number of perturbations to generate for each prompt.
+        instructions (str): Instructions for response generation.
+
+    Methods:
+        run_pipeline(): Runs the LLM analysis pipeline and returns the processed data.
+
+    """
+
     def __init__(self, input_data, models_dict, perturbation_model, llm_evaluation_model, temperature, 
     num_runs, is_file_path, similarity_model_name, num_perturbations, instructions):
         self.num_runs = num_runs
@@ -282,6 +543,13 @@ class LLMAnalysisPipeline:
         self.llm_rating_calculator = LLMRatingCalculator(llm_evaluation_model)
 
     def run_pipeline(self):
+        """
+        Runs the LLM analysis pipeline.
+
+        Returns:
+            pandas.DataFrame: Processed data with calculated metrics.
+
+        """
         df = self.data_loader.load_data()
         all_prompts = df["prompt"].unique()
         perturbations_dict = self.perturbation_generator.get_perturbations_for_all_prompts(all_prompts)
@@ -294,6 +562,17 @@ class LLMAnalysisPipeline:
         return df_responses
 
 def aggregate_best_scores(df, score_column):
+    """
+    Aggregates the best scores from a DataFrame based on a specified score column.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame containing the scores.
+        score_column (str): The name of the column containing the scores.
+
+    Returns:
+        pandas.DataFrame: The DataFrame with the best scores for each group.
+
+    """
     # Define the columns to include in the output
     relevant_columns = ['model', 'original_prompt', 'actual_prompt', 'response', 'true_or_false', score_column]
 
@@ -305,8 +584,10 @@ def aggregate_best_scores(df, score_column):
 
     return df_best_scores
 
-
 def del_file():
+    """
+    Deletes all files with the prefix 'litellm_' in the current directory.
+    """
     temp_files = glob.glob('litellm_*')
     for file in temp_files:
         os.remove(file)
