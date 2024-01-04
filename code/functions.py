29dbb32e400382dcb0980d1d277755e360dda57f
abigailhaddad <abigail.haddad@gmail.com>
2024-01-04 13:41:39 -0500

    handle perturbation num_perturbations in tests

--- code/functions.py
+++ code/functions.py
@@ -670,13 +670,13 @@ class LLMAnalysisPipeline:
         temperature,
         is_file_path,
         stability_threshold,
-        num_perturbations=10,
+        num_perturbations= 10
     ):
         self.data_loader = DataLoader(input_data, is_file_path)
         self.temperature = temperature
         self.perturbation_generator = PerturbationGenerator(
-            perturbation_model[0], perturbation_model[1], num_perturbations
-        )
+    perturbation_model[0], perturbation_model[1], num_perturbations
+)
 
         # Create calculator instances
         self.similarity_calculator = SimilarityCalculator(similarity_model_name)
