4b093afb02cb464178345df83bc2684485f9166c
abigailhaddad <abigail.haddad@gmail.com>
2024-02-01 11:00:21 -0500

    adds perturbations in workbook, tweaks logging

--- code/functions.py
+++ code/functions.py
@@ -1384,16 +1384,10 @@ class OpenEndedTextAnalysisPipeline:
         self.num_perturbations = num_perturbations
 
     def process_all_prompts_models(self):
-        """
-        Processes all prompts and generates responses.
-
-        Returns:
-            pd.DataFrame: DataFrame containing the generated responses.
-        """
         perturbation_generator = PerturbationGenerator(
             self.perturbation_model, self.provider, self.num_perturbations
         )
-        perturbations_dict = perturbation_generator.get_perturbations_for_all_prompts(
+        self.perturbations_dict = perturbation_generator.get_perturbations_for_all_prompts(
             self.prompts
         )
 
@@ -1401,10 +1395,12 @@ class OpenEndedTextAnalysisPipeline:
         for model, model_provider in self.models_dict.items():
             for prompt in self.prompts:
                 responses = self.generate_responses_for_prompt(
-                    model, model_provider, prompt, perturbations_dict
+                    model, model_provider, prompt, self.perturbations_dict
                 )
                 all_responses.extend(responses)
+
         return pd.DataFrame(all_responses)
+
 
     def generate_responses_for_prompt(
         self, model, provider, prompt, perturbations_dict
